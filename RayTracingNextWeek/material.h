9ebf5cac388162a4cfd89744cf53e3c950c9825b
yangyuan333 <347065418@qq.com>
2022-10-05 20:46:52 +0800

    fix a stupid bug

--- RayTracingNextWeek/material.h
+++ RayTracingNextWeek/material.h
@@ -91,7 +91,7 @@ public:
 class diffuse_light : public material {
 public:
     diffuse_light(shared_ptr<texture> a) : emit(a) {}
-    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+    diffuse_light(const color& c) : emit(make_shared<solid_color>(c)) {}
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
